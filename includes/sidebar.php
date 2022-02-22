e0a7d706d29f0ecf2612df330c94e0e12c4d10ee
sanjanar06 <sanjanareji06@gmail.com>
2022-02-22 11:54:56 +0530

    FIxed the echo bug

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,32 +1,3 @@
-<?php
-
-        if(isset($_POST['submit'])){
-
-            $search = $_POST['search'];
-
-            $query="SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
-            $search_query = mysqli_query($connection , $query);
-
-            if(!$search_query){
-
-                die("QUERY FAILED". mysqli_error($connection));
-
-            }
-
-            $count = mysqli_num_rows($search_query);
-
-            if($count == 0){
-                echo "<h1 NO RESULT </h1>";
-            }
-            // else{
-            //     echo "SOME RESULTS";
-            // }
-           
-            
-        }
-
-
-?>
 
 <!-- Blog Sidebar Widgets Column -->
 <div class="col-md-4">
@@ -35,11 +6,11 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="POST">
+        <form action="search.php" method="post">
             <div class="input-group">
                 <input name="search" type="text" class="form-control">
                 <span class="input-group-btn">
-                    <button name="submit" class="btn btn-default" type="button">
+                    <button name="submit" class="btn btn-default" type="submit">
                         <span class="glyphicon glyphicon-search"></span>
                 </button>
                 </span>
